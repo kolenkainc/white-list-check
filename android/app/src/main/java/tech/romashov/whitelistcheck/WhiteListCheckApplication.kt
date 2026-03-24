a8e4487290be2648d03b4b0146375bce18983281
Dmitry Romashov <framebassman@gmail.com>
2026-03-24 11:14:17 +0100

    Add sentry

---
+++ android/app/src/main/java/tech/romashov/whitelistcheck/WhiteListCheckApplication.kt
@@ -0,0 +1,18 @@
+package tech.romashov.whitelistcheck
+
+import android.app.Application
+import io.sentry.android.core.SentryAndroid
+
+class WhiteListCheckApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val dsn = BuildConfig.SENTRY_DSN
+        if (dsn.isBlank()) return
+        SentryAndroid.init(this) { options ->
+            options.dsn = dsn
+            options.environment = if (BuildConfig.DEBUG) "debug" else "production"
+            options.isDebug = BuildConfig.DEBUG
+        }
+    }
+}
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("io.sentry.android.gradle") version "4.14.1" apply false
 }
